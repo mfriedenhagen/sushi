0fd74021647a465b2e7786dbc1c7d70daaea8942
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2015-10-07 23:09:59 +0200

    WIP

--- src/main/java/net/oneandone/sushi/junit/XmlRunListener.java
+++ src/main/java/net/oneandone/sushi/junit/XmlRunListener.java
@@ -1,14 +1,16 @@
 package net.oneandone.sushi.junit;
 
+import org.junit.runner.Description;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.nio.charset.Charset;
+import java.util.Locale;
 
 /**
  * Created by mirko on 07.10.15.
@@ -23,19 +25,50 @@ public class XmlRunListener extends RunListener {
 
     final XMLStreamWriter writer;
 
-    public XmlRunListener(OutputStream out){
+    private final ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+    public XmlRunListener(OutputStream out, String testSuiteName){
         this.out = out;
         try {
             writer = XMLOutputFactory.newInstance().createXMLStreamWriter(out, "utf-8");
-            writer.writeProcessingInstruction("xml version=\"1.0\" encoding=\"UTF-8\"");
+            writer.writeStartDocument("UTF-8", "1.0");
+            writer.writeStartElement("testsuite");
+            writer.writeAttribute("name", testSuiteName);
         } catch (XMLStreamException e) {
             throw new RuntimeException("Could not create XMLStreamWriter", e);
         }
     }
 
     @Override
+    public void testStarted(Description description) throws Exception {
+        super.testStarted(description);
+        writer.writeStartElement("testcase");
+        writer.writeAttribute("name", description.getDisplayName());
+        writer.writeAttribute("classname", description.getClassName());
+        startTime.set(System.currentTimeMillis());
+    }
+
+    @Override
+    public void testFinished(Description description) throws Exception {
+        super.testFinished(description);
+        writer.writeAttribute("time",
+                String.format(Locale.ENGLISH, "%.3f", (System.currentTimeMillis() - startTime.get())/1000.0));
+        writer.writeEndElement();
+    }
+
+    @Override
+    public void testFailure(Failure failure) throws Exception {
+        super.testFailure(failure);
+        writer.writeStartElement("failure");
+        writer.writeAttribute("message", failure.getMessage());
+        writer.writeAttribute("type", failure.);
+    }
+
+    @Override
     public void testRunFinished(Result result) throws Exception {
         super.testRunFinished(result);
+        writer.writeEndElement();
+        writer.writeEndDocument();
         writer.close();
     }
 }
--- src/test/java/net/oneandone/sushi/junit/XmlRunListenerTest.java
+++ src/test/java/net/oneandone/sushi/junit/XmlRunListenerTest.java
@@ -1,9 +1,17 @@
 package net.oneandone.sushi.junit;
 
+import net.oneandone.sushi.io.BufferTest;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.JUnitCore;
 
 import java.io.ByteArrayOutputStream;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
@@ -15,8 +23,24 @@ public class XmlRunListenerTest {
 
     @Test
     public void testTestRunFinished() throws Exception {
-        final XmlRunListener sut = new XmlRunListener(out);
+        final XmlRunListener sut = new XmlRunListener(out, "testsuite-name");
+        final Description description = Description.createTestDescription(XmlRunListenerTest.class.getName(), "foo");
+        sut.testStarted(description);
+        Thread.sleep(50);
+        sut.testFinished(description);
         sut.testRunFinished(null);
-        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", out.toString("utf-8"));
+        final String actual = out.toString("utf-8");
+        assertThat(actual, startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?><testsuite name=\"testsuite-name\"><testcase name=\"foo(net.oneandone.sushi.junit.XmlRunListenerTest)\" classname=\"net.oneandone.sushi.junit.XmlRunListenerTest\" time=\"0.05"));
+        assertThat(actual, endsWith("</testsuite>"));
+    }
+
+    @Test
+    public void testRun() throws Exception {
+        final XmlRunListener sut = new XmlRunListener(out, "testsuite-name");
+        final JUnitCore core = new JUnitCore();
+        core.addListener(sut);
+        core.run(BufferTest.class);
+        final String actual = out.toString();
+        assertThat(actual, containsString("testcase name=\"eadLine("));
     }
 }
